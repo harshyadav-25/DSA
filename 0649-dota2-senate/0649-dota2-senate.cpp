256f99e932786a18acccb5617962d26fa4ac2be3
Harsh Yadav <2k24.csaiml1b.2413391@gmail.com>
2026-07-26 13:41:14 +0530

    Time: 3 ms (64.45%), Space: 11.7 MB (33.61%) - LeetHub

--- 0649-dota2-senate/0649-dota2-senate.cpp
+++ 0649-dota2-senate/0649-dota2-senate.cpp
@@ -2,52 +2,54 @@ class Solution {
 public:
     string predictPartyVictory(string senate) {
         int n = senate.length();
-        int Rc = 0;
-        int Dc = 0;
-        for(char c: senate){
-            if(c == 'R') Rc++;
-            else Dc++;
+        queue<int>q;
+        queue<int>r;
+        queue<int>d;
+        for(int i = 0; i < n; i++){
+            q.push(i);
+            if(senate[i] == 'R') r.push(i);
+            if(senate[i] == 'D') d.push(i);
         }
-        if(Rc == 0) return "Dire";
-        if(Dc == 0) return "Radiant";
-        queue<char>q;
-        int Rban = 0;
-        int Dban = 0;
-        for(char s: senate){
-            q.push(s);
-        }
-        while(Rc > 0 && Dc >0){
-            char curr = q.front();
-            q.pop();
-            if(curr == 'R'){
-                if(Rban > 0){
-                    Rban--;
-                    Rc--;
-                }
-            
+        while(q.size() > 1){
+        
+            if(r.size() == 0) return "Dire";
+            if(d.size() == 0) return "Radiant";
+
+            if(senate[q.front()] == 'X') q.pop();
+            else if(senate[q.front()] == 'R'){
+                //check for victory
+                if(d.size() == 0) return "Radiant";
                 else{
-                    Dban++;
-                    q.push('R');
+                    senate[d.front()] = 'X';
+                    d.pop();
+                    q.push(q.front());
+                    q.pop();
+                    
+                    r.push(r.front());
+                    r.pop();
                     
                 }
             }
-            else{//D
-                if(Dban > 0){
-                    Dban--;
-                    Dc--;
+            else{ //'D
+                if(r.size() == 0) return "Dire";
+                else{
+                    
+                senate[r.front()] = 'X';
+                r.pop();
+                q.push(q.front());
+                q.pop();
+                d.push(d.front());
+                d.pop();
 
                 }
-                else{
-                    Rban++;
-                    q.push('D');
-                }
+
 
 
             }
-        
         }
-        if(Rc > 0) return "Radiant";
-        else return "Dire";
+        if(senate[q.front()] == 'R') return "Radiant";
+        else return "Dire"; 
+
         
     }
 };
