b689ca8e1ad1be8d90f168fe4d544ffd0d092b64
Harsh Yadav <2k24.csaiml1b.2413391@gmail.com>
2026-08-05 23:09:35 +0530

    Time: 12 ms (42.14%), Space: 11.9 MB (23.72%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -11,16 +11,27 @@ public:
     bool hasCycle(ListNode *head) {
         ListNode* slow = head;
         ListNode* fast = head;
+        bool flag = false;
         while(fast!= NULL&& fast->next!=NULL){
             slow = slow->next;
             fast = fast->next->next;
-            if(slow == fast) return true;
+            if(slow == fast){
+                flag = true;
+                break;
+            }
            
-            
-
+        }
+        if(flag == false) return NULL;
+        else{
+            ListNode* temp = head;
+            while(temp != slow){
+                slow = slow->next;
+                temp = temp->next;
+            }
+            return slow;
         }
          
-        return false;
+       
         
     }
 };
