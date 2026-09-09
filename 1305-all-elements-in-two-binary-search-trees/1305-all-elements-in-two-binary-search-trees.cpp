bc091d980b11329c400ba4f3e97a4e6a3bded8e2
Harsh Yadav <2k24.csaiml1b.2413391@gmail.com>
2026-09-09 10:20:53 +0530

    Time: 0 ms (100%), Space: 88.6 MB (10.93%) - LeetHub

---
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    void inorder(TreeNode* root, vector<int>& ans) {
+        if (root == NULL) return;
+
+        inorder(root->left, ans);
+        ans.push_back(root->val);
+        inorder(root->right, ans);
+    }
+
+    vector<int> getAllElements(TreeNode* root1, TreeNode* root2) {
+        vector<int> ans1, ans2, ans;
+
+        inorder(root1, ans1);
+        inorder(root2, ans2);
+
+        int i = 0, j = 0;
+
+        while (i < ans1.size() && j < ans2.size()) {
+            if (ans1[i] <= ans2[j])
+                ans.push_back(ans1[i++]);
+            else
+                ans.push_back(ans2[j++]);
+        }
+
+        while (i < ans1.size())
+            ans.push_back(ans1[i++]);
+
+        while (j < ans2.size())
+            ans.push_back(ans2[j++]);
+
+        return ans;
+    }
+};
